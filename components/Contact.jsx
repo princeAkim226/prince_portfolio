3ca10848f591e5fba39d0941d0d221803b63466e
Prince Akim <siathomas13@gmail.com>
2023-02-21 10:52:45 +0000

    change form

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -75,8 +75,9 @@ const Contact = () => {
           <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
             <div className='p-4'>
               <form
-                action='https://getform.io/f/08ebcd37-f5b5-45be-8c13-714f011ce060'
-                method='POST'
+
+                action="https://formsubmit.co/d0bf993209ae2f849816ee2229192e68 "
+                method="POST"
                 encType='multipart/form-data'
               >
                 <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
