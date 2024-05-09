8ca598d39e630a7a96a0846bf782efce37ddf195
princeAkim226 <103947397+princeAkim226@users.noreply.github.com>
2024-05-09 11:48:51 +0000

    Update Projects.jsx

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -17,7 +17,7 @@ const Projects = () => {
         <h2 className='py-4'>What I&apos;ve Built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
           <ProjectItem
-            title='Property Finder'
+            title='Resumons les articles '
             backgroundImg={propertyImg}
             projectUrl='/property'
             tech='React JS'
